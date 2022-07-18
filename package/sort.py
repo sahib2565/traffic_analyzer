d818082de03439d73f97002e22372db671fdbd2d
Sahib Singh <85299873+sahib2565@users.noreply.github.com>
2022-07-18 13:44:05 +0200

    Update sort.py
    Risolto il problema del concatenate.

--- package/sort.py
+++ package/sort.py
@@ -242,17 +242,16 @@ class Sort(object):
     i = len(self.trackers)
     for trk in reversed(self.trackers):
         d = trk.get_state()[0]
+        print(d)
         if (trk.time_since_update < 1) and (trk.hit_streak >= self.min_hits or self.frame_count <= self.min_hits):
-          ret.append(np.concatenate((d,[trk.id+1])).reshape(1,-1)) # +1 as MOT benchmark requires positive
+          print(ret)
+          print(trk.id+1)
+          ret.append(np.concatenate((d,[trk.id+1,dets[0][5]])).reshape(1,-1)) # +1 as MOT benchmark requires positive
         i -= 1
         # remove dead tracklet
         if(trk.time_since_update > self.max_age):
           self.trackers.pop(i)
     if(len(ret)>0):
-      print("ID!")
-      print(dets[0][5])
-      print(ret)
-      ret.append(np.concatenate((dets[0][5])).reshape(1,-1))
       return np.concatenate(ret)
     return np.empty((0,5))
 
